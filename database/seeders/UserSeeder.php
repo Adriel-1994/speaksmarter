c4c284103b93bffef812ba848ae7ca986bdc97f1
Adriel García Díaz <adriel.garcia102@gmail.com>
2025-01-05 01:48:36 +0000

    UPD - Add jetstream and inertia for the VUE frontend. UPD - Create RoleSeeder.php and UserSeeder.php for the default values. UPD - Add new Seeders to DatabaseSeeder.php UPD - Add HasRoles on User model.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@speaksmarter.com',
+            'password' => Hash::make('admin'),
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'editor',
+            'email' => 'editor@speaksmarter.com',
+            'password' => Hash::make('editor'),
+        ])->assignRole('editor');
+    }
+}
